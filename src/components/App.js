dfaab54f434b2aa6f491ff31365bda70d61d829c
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-07-20 10:37:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ function App() {
 }
   return (
     <div id="main">
-      <button id="click" onClick="renderPara"></button>
+      <button id="click" onClick="renderPara">Click</button>
   {
     para && ( <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>)
   }
